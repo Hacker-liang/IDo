0d3ce7e0a168eaf529ec28b38fe6d0809ea4744d
Hacker-liang <pengshuai.liang@gmai.com>
2015-09-23 13:03:38 +0800

    init

---
+++ IDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDo
+//
+//  Created by liangpengshuai on 9/23/15.
+//  Copyright © 2015 com.Yinengxin.xianne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
