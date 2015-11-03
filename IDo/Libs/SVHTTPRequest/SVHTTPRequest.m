7e7a29cf0b998d98a0a1e42a55a739122f3c716d
Hacker-liang <pengshuai.liang@gmai.com>
2015-11-03 10:36:29 +0800

    启动页面

--- IDo/Libs/SVHTTPRequest/SVHTTPRequest.m
+++ IDo/Libs/SVHTTPRequest/SVHTTPRequest.m
@@ -117,7 +117,9 @@ static NSString *defaultUserAgent;
 + (SVHTTPRequest*)GET:(NSString *)address parameters:(NSDictionary *)parameters completion:(SVHTTPRequestCompletionHandler)block {
     SVHTTPRequest *requestObject = [[self alloc] initWithAddress:address method:SVHTTPRequestMethodGET parameters:parameters saveToPath:nil progress:nil completion:block];
     if ([UserManager shareUserManager].userInfo.token) {
-        [requestObject setValue:[UserManager shareUserManager].userInfo.token forHTTPHeaderField:@"token"];
+        if ([[UserManager shareUserManager].userInfo.token length] > 1) {
+            [requestObject setValue:[UserManager shareUserManager].userInfo.token forHTTPHeaderField:@"token"];
+        }
         [requestObject setValue: @"2" forHTTPHeaderField:@"deviceType"];
         NSDictionary *infoDict =[[NSBundle mainBundle] infoDictionary];
         NSString *versionNum =[infoDict objectForKey:@"CFBundleShortVersionString"];
@@ -144,7 +146,9 @@ static NSString *defaultUserAgent;
 + (SVHTTPRequest*)POST:(NSString *)address parameters:(NSObject *)parameters completion:(SVHTTPRequestCompletionHandler)block {
     SVHTTPRequest *requestObject = [[self alloc] initWithAddress:address method:SVHTTPRequestMethodPOST parameters:parameters saveToPath:nil progress:nil completion:block];
     if ([UserManager shareUserManager].userInfo.token) {
-        [requestObject setValue:[UserManager shareUserManager].userInfo.token forHTTPHeaderField:@"token"];
+        if ([[UserManager shareUserManager].userInfo.token length] > 1) {
+            [requestObject setValue:[UserManager shareUserManager].userInfo.token forHTTPHeaderField:@"token"];
+        }
         [requestObject setValue: @"2" forHTTPHeaderField:@"deviceType"];
         NSDictionary *infoDict =[[NSBundle mainBundle] infoDictionary];
         NSString *versionNum =[infoDict objectForKey:@"CFBundleShortVersionString"];
