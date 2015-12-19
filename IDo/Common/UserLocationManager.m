f0fd637f0c618714f00827f7f3ef660c2916a968
Hacker-liang <pengshuai.liang@gmai.com>
2015-12-19 14:41:29 +0800

    定位 bug

--- IDo/Common/UserLocationManager.m
+++ IDo/Common/UserLocationManager.m
@@ -50,7 +50,7 @@
 - (void)updateUserLocation
 {
     if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
-        [self.locationManager requestWhenInUseAuthorization];
+        [self.locationManager requestAlwaysAuthorization];
     }
 
     [self.locationManager startUpdatingLocation];
